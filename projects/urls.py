ddb23e783ce76c4592236c745c2c346f0842d2fc
Aqite93 <Aqite93@gmail.com>
2019-10-02 23:24:18 +0900

    fix project's constuction.

--- projects/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import login_view
-
-urlpatterns = [
-    path("", login_view.project_index, name="project_index"),
-    path("<int:pk>/", login_view.project_detail, name="project_detail"),
-]
