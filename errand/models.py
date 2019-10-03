427c3ec66ab597fd5e7366dbc87659b7eda20ab9
Aqite93 <Aqite93@gmail.com>
2019-10-03 16:42:38 +0900

    add fixture for errand.

--- errand/models.py
+++ errand/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Errand(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    errand_id = models.AutoField(primary_key=True)
+    description = models.TextField(max_length=1024,
+                                   blank=True,
+                                   help_text='依頼内容を記載してください．')
+
+    class Meta:
+        db_table = 'errand'
