043a939d437afefbf4d9c531f3f4e1d04745a803
Aqite93 <Aqite93@gmail.com>
2019-10-03 15:03:13 +0900

    add to check user.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from django.urls import path
-from django.urls import include
 from .views import LoginView
 
 urlpatterns = [
-    path('', LoginView.as_view(), name='login'),
+    path('', LoginView.as_view()),
     path('login/', LoginView.as_view(), name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from .models import User
+from django.shortcuts import render, redirect
 from django.views import View
+
 from .forms import UserForm
+from .models import User
 
 
 class LoginView(View):
@@ -11,3 +12,16 @@ class LoginView(View):
         return render(request, 'login.html', context={
             'form': form
         })
+
+    def post(self, request):
+        form = UserForm(request.POST)
+        email = request.POST.get('email', None)
+        password = request.POST.get('password', None)
+
+        if not User.objects.filter(email=email, password=password).exists():
+            return render(request, 'login.html', context={
+                'form': form,
+                'message': '入力した情報が正しくありません。内容を確認の上、再度入力してください。'
+            })
+
+        return redirect(request, 'login')
