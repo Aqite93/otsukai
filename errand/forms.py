c5b2b0bd3eb675bd841aa6145a42c135b4d797d9
Aqite93 <Aqite93@gmail.com>
2019-10-21 17:05:08 +0900

    add form to registre errand.

--- errand/forms.py
+++ errand/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Errand
+from .models import Errand, Category
 
 
 class ErrandIndexForm(forms.ModelForm):
@@ -17,3 +17,34 @@ class ErrandIndexForm(forms.ModelForm):
 ErrandIndexFormSet = forms.modelformset_factory(
     Errand, form=ErrandIndexForm, extra=0
 )
+
+
+class ErrandRegisterForm(forms.ModelForm):
+    category = forms.ModelChoiceField(
+        label='Category',
+        queryset=Category.objects.all()
+    )
+
+    deadline = forms.DateTimeField(
+        label='Deadline',
+        required=True,
+        widget=forms.DateInput(attrs={"type": "date"}),
+        input_formats=['%Y-%m-%d']
+    )
+
+    price = forms.IntegerField(
+        label='Price',
+        widget=forms.Textarea()
+    )
+
+    image = forms.FileField()
+
+    comments = forms.CharField(
+        label='Description',
+        max_length=1024,
+        widget=forms.Textarea()
+    )
+
+    class Meta:
+        model = Errand
+        fields = ('description',)
